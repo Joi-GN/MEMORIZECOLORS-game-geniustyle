a1f4fa64a1b09d26f3622c5c9c3eeaf3f0f28851
Joi-GN <joicegomesdn@gmail.com>
2022-03-26 16:05:01 -0300

    adjustments, accessibility added, gifs added

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Projeto desenvolvido para o desafio criando seu jogo da memória estilo Genius.
 
+![](video1.gif)
+
 O jogo foi desenvolvido com HTML, CSS e JavaScript e pode ser acessado por um navegador de internet.
 
-![]()
+## Acessibilidade
+
+No jogo também está inclusa uma função de acessibilidade para daltônicos.
+
+![](gif1.gif)
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// MEMORIZE COLOR GAME GENIUS/SIMON STYLE
+// MEMORIZE COLORS GAME GENIUS/SIMON STYLE
 // DEV JOICE GOMES
 // 2022
 
@@ -11,65 +11,53 @@ const blue = document.querySelector('.blue');
 // Variables
 let colorOrder = [];
 let clickedColorOrder = [];
-let level = 0;
+let level = 1;
+
+//Confirmation to start game 
+setTimeout(startGame, 3000);
+
+function startGame() {
+    let welcome = 'Bem vindo ao Memorize Colors!\nObserve as cores e clique na sequência de cores que iluminarem!';
+    if (confirm(welcome) == true) {
+        playGame()
+    } else {
+        alert('Até mais!');
+    }
+
+}
 
 // Initiate game
-playGame();
-
-function playGame(){
-    alert('Bem vindo ao Memorize Colors!');
-    level = 0;
+function playGame() {
+    level = 1;
     randomColorOrder();
 }
 
-// Get User's click
-function getClick(color){
-    clickedColorOrder[clickedColorOrder.length] = color;
-    createColorElement(color).classList.add('selected');
-
-    setTimeout(() => {
-        createColorElement(color).classList.remove('selected');
-        checkColorOrder();
-    },250)
-}
-
-green.onclick = () => getClick(0);
-red.onclick = () => getClick(1);
-yellow.onclick = () => getClick(2);
-blue.onclick = () => getClick(3);
-
 // Generate random color order
-function randomColorOrder(){
-    let colorNumber = Math.floor(Math.random()*4)
+function randomColorOrder() {
+    let colorNumber = Math.floor(Math.random() * 4)
     colorOrder[colorOrder.length] = colorNumber;
     clickedColorOrder = [];
 
-    for(let i in colorOrder){
+    for (let i in colorOrder) {
         let elementColor = createColorElement(colorOrder[i]);
-        lightenColor(elementColor, Number(i)+1);
+        lightenColor(elementColor, Number(i) + 1);
     }
 }
 
-function createColorElement(color){
-    if(color == 0){
+function createColorElement(color) {
+    if (color == 0) {
         return green;
-    }else if(color == 1){
+    } else if (color == 1) {
         return red;
-    }else if(color == 2){
+    } else if (color == 2) {
         return yellow;
-    }else if(color == 3){
+    } else if (color == 3) {
         return blue;
     }
 }
 
-// Next Level
-function nextLevel(){
-    level++;
-    randomColorOrder();
-}
-
 // Lighten color
-function lightenColor(element, time){
+function lightenColor(element, time) {
     time = time * 500;
 
     setTimeout(() => {
@@ -81,25 +69,61 @@ function lightenColor(element, time){
     }, time);
 }
 
-// Check if color order is right 
-function checkColorOrder(){
-    for(let i in clickedColorOrder){
-        if(clickedColorOrder[i] != colorOrder[i]){
+// Get User's click
+function getClick(color) {
+    clickedColorOrder[clickedColorOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkColorOrder();
+    }, 250)
+}
+
+green.onclick = () => getClick(0);
+red.onclick = () => getClick(1);
+yellow.onclick = () => getClick(2);
+blue.onclick = () => getClick(3);
+
+// Check if clicked colors order is right 
+function checkColorOrder() {
+    for (let i in clickedColorOrder) {
+        if (clickedColorOrder[i] != colorOrder[i]) {
             gameOver();
             break;
         }
     }
-
-    if(clickedColorOrder.length == colorOrder.length){
-        alert(`Nível: ${level} \nParabéns! Você passou para o próximo nível!`);
-        nextLevel();
+    if (colorOrder.length >= 1){
+        if (clickedColorOrder.length == colorOrder.length) {
+            alert(`Nível: ${level} \nParabéns! Você passou para o próximo nível!`);
+            nextLevel();
+        }
     }
 }
 
+// Next Level
+function nextLevel() {
+    level++;
+    randomColorOrder();
+}
+
 // Game Over
-function gameOver(){
-    alert(`Game Over\nVocê errou a ordem! \nNível: ${level}\nClique em Ok para iniciar um novo jogo`)
-    colorOrder = [];
-    clickedColorOrder = [];
-    playGame();
+function gameOver() {
+    alert(`Game Over\nVocê errou a ordem! \nNível: ${level}`)
+    if (confirm('Gostaria de jogar denovo?') == true) {
+        colorOrder = [];
+        clickedColorOrder = [];
+        playGame()
+    } else {
+        alert('Até mais!');
+        colorOrder = [];
+        clickedColorOrder = [];
+        setTimeout(startGame, 2000);
+    }
+}
+
+// Change colors mode - accessibility
+function changeMode() {
+    const game = document.querySelector('.game');
+    game.classList.toggle('accessibility');
 }
